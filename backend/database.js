d0b61a36a5072da03ce099c22d9130cd728e0d7a
Kaushal Sudarshana <90532245+92063801@users.noreply.github.com>
2023-11-14 23:00:15 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -10,7 +10,7 @@ dbConnection
   });
 
 let _db;
-
+//ty
 function init(db) {
     _db = db;
 }
@@ -18,9 +18,8 @@ function init(db) {
 const dbinitialize = async () => {
     testBase.resetDatabase(knex_db);
 }
-
 const readTeachers = async () => {
-    const sql = `SELECT * FROM teacher`
+    const sql = `SELECT * FROM teacher `
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
@@ -28,30 +27,45 @@ const readTeachers = async () => {
             });
     });
 }
-
-const readTeacherInfo = async (id) => {
-    const sql = `SELECT * FROM teacher WHERE id = ?`
+const addTeacher = async (id, name, age) => {
+    const sql = "INSERT INTO teacher(id, name, age) VALUES("+'"'+id+'", '+'"'+ name+'",  '+'"'+ age+'")'
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [id])
-            .then((teacher) => {
-                resolve(teacher);
+            .raw(sql)
+            .then((data) => {
+                resolve(data)
             })
             .catch((error) => {
                 reject(error);
-	@@ -36,7 +49,7 @@ const addTeacher = async (id, name, age) => {
+            });
+    });
+}
+
+
+
+const readTeacherInfo = async (id) => {
+    const sql = "SELECT * FROM teacher WHERE id="+id
+    return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [id, name, age])
-            .then(() => {
-                resolve({status: "Successfully inserted Teacher"});
+            .raw(sql)
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
-	@@ -50,12 +63,11 @@ const updateTeacher = async (name, age, id) => {
+            });
+    });
+}
+
+
+
+const updateTeacher = async (name, age, id) => {
+    const sql = "UPDATE teacher SET name="+'"'+name+'"'+','+" age="+'"'+age+'"'+" WHERE id="+id
+    return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [name, age, id])
-            .then(() => {
-                resolve({status: "Successfully updated Teacher"});
+            .raw(sql)
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
@@ -59,11 +73,12 @@ const readTeacherInfo = async (id) => {
 }
 
 const deleteTeacher = async (id) => {
-	@@ -64,35 +76,48 @@ const deleteTeacher = async (id) => {
+    const sql = "DELETE FROM teacher WHERE id="+id
+    return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [id])
-            .then(() => {
-                resolve({status: "Successfully deleted Teacher"});
+            .raw(sql)
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
@@ -71,13 +86,15 @@ const deleteTeacher = async (id) => {
     });
 }
 
+// ============== Student  Related implementation ==============
+
 const readStudents = async () => {
-    const sql = `SELECT * FROM student`
+    const sql = `SELECT * FROM student `
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
-            .then((students) => {
-                resolve(students);
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
@@ -85,12 +102,12 @@ const readStudents = async () => {
 }
 
 const readStudentInfo = async (id) => {
-    const sql = `SELECT * FROM student WHERE id = ?`
+    const sql = "SELECT * FROM student WHERE id="+id
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
-            .then((student) => {
-                resolve(student);
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
@@ -98,21 +115,27 @@ const readStudentInfo = async (id) => {
     });
 }
 
-const addStudent = async (id, name, age, hometown) => {
-    const sql = `INSERT INTO student(id,name,age,hometown) values (?, ?, ?, ?)`
+const addStudent = async (id, name, age, religion) => {
+    const sql = "INSERT INTO student(id, name, age, hometown) VALUES("+'"'+id+'", '+'"'+ name+'",  '+'"'+ age+'", '+'"'+religion+'")'
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [id, name, age, hometown])
-            .then(() => {
-                resolve({status: "Successfully inserted Student"});
+            .raw(sql)
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
-	@@ -106,41 +131,35 @@ const updateStudent = async (name, age, hometown, id) => {
+            });
+    });
+}
+
+const updateStudent = async (name, age, religion, id) => {
+    const sql = "UPDATE student SET name="+'"'+name+'"'+','+" age="+'"'+age+'", hometown='+'"'+religion+'"'+" WHERE id="+id
+    return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [name, age, hometown, id])
-            .then(() => {
-                resolve({status: "Successfully updated Student"});
+            .raw(sql)
+            .then((data) => {
+                resolve(data)
             })
             .catch((error) => {
                 reject(error);
@@ -120,12 +143,12 @@ const addStudent = async (id, name, age, hometown) => {
 } 
 
 const deleteStudent = async (id) => {
-    const sql = `DELETE FROM student WHERE id = ?`
+    const sql = "DELETE FROM student WHERE id="+id
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [id])
-            .then(() => {
-                resolve({status: "Successfully deleted Student"});
+            .raw(sql)
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => {
                 reject(error);
